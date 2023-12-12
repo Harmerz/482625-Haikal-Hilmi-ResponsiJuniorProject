db90681d2e953a46424834e0943062433e04fe33
Haikal Hilmi <Haikal Hilmi@DESKTOP-QBUSUMM>
2023-12-12 09:28:20 +0700

    feat : Logo Image

--- 482625-Haikal Hilmi-ResponsiJuniorProject/Form1.Designer.cs
+++ 482625-Haikal Hilmi-ResponsiJuniorProject/Form1.Designer.cs
@@ -45,7 +45,9 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvData
@@ -62,7 +64,7 @@
             // 
             this.Logo.AutoSize = true;
             this.Logo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Logo.Location = new System.Drawing.Point(48, 12);
+            this.Logo.Location = new System.Drawing.Point(72, 18);
             this.Logo.Name = "Logo";
             this.Logo.Size = new System.Drawing.Size(120, 20);
             this.Logo.TabIndex = 1;
@@ -204,7 +206,7 @@
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(116, 20);
             this.label9.TabIndex = 16;
-            this.label9.Text = "PM : Proudct M";
+            this.label9.Text = "PM : Product M";
             // 
             // label10
             // 
@@ -216,11 +218,22 @@
             this.label10.TabIndex = 17;
             this.label10.Text = "FIN : Finance";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_482625_Haikal_Hilmi_ResponsiJuniorProject.Properties.Resources.Logo_UGM;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 46);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(532, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -244,6 +257,7 @@
             this.Load += new System.EventHandler(this.LoadData);
             this.Shown += new System.EventHandler(this.LoadData);
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -268,6 +282,7 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
